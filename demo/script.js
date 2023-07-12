0c26029e8b4bcda8debc06978277bda87bfbfbae
Emiliano Hotes <emiliano.hotes@despegar.com>
2023-07-12 19:46:12 -0300

    feat: created first deck

--- demo/script.js
+++ demo/script.js
@@ -1,1 +1,12 @@
 // Script
+
+window.document.addEventListener("DOMContentLoaded", () => {
+  console.log("test")
+
+  let card = document.getElementById("test");
+
+  console.log(card)
+
+  card.setAttribute("x","100")
+  card.setAttribute("y","200")
+})
